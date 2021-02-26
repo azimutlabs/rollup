94425905723017e4d0867fbc0f48083ba8f5f760
olehan <alihan.arshabekov@gmail.com>
2021-02-26 15:05:54 +0600

    fix(rollup-config): reverse array to filter plugins in combine

--- packages/rollup-config/src/services/combine.ts
+++ packages/rollup-config/src/services/combine.ts
@@ -1,19 +1,24 @@
 import type { RollupConfigFinalize } from '../types/RollupConfigFinalize';
+import { getCurrentEnv } from './getCurrentEnv';
 import { merge } from './merge';
 
 export const combine = (
   // eslint-disable-next-line functional/functional-parameters
   ...configs: readonly [RollupConfigFinalize, ...(readonly RollupConfigFinalize[])]
-): RollupConfigFinalize => (dirname, env) => {
+): RollupConfigFinalize => (dirname, env = getCurrentEnv()) => {
   const [first, ...rest] = configs.map((cfg) => cfg(dirname, env));
   const config = merge([first, ...rest]);
-  const configSet = new Set<string>(config.plugins?.map((plg) => plg.name));
-  const plugins = config.plugins?.filter(({ name }) => {
-    if (configSet.has(name)) {
-      configSet.delete(name);
+  const configSet = new Set<string>();
+  const plugins = config.plugins
+    ?.slice()
+    .reverse()
+    .filter(({ name }) => {
+      if (!configSet.has(name)) {
+        configSet.add(name);
+        return true;
+      }
       return false;
-    }
-    return true;
-  });
+    })
+    .reverse();
   return { ...config, plugins };
 };
