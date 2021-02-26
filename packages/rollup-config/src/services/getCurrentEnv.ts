e12d93ab4bfe9540f996d98ff8e891271bec5b38
Nursultan Akhmetzhanov <nursultan1998@gmail.com>
2021-02-26 11:26:04 +0600

    fix(rollup-config): remove All option from current environment

--- packages/rollup-config/src/services/getCurrentEnv.ts
+++ packages/rollup-config/src/services/getCurrentEnv.ts
@@ -1,10 +1,12 @@
 import { Envs } from './types/Envs';
 
 const envs: Record<string, Envs> = {
-  'development': Envs.Dev,
-  'production': Envs.Prod,
-  'test': Envs.Test,
-  '': Envs.All,
+  development: Envs.Dev,
+  production: Envs.Prod,
+  test: Envs.Test,
 };
 
-export const getCurrentEnv = (env = process.env.NODE_ENV ?? ''): Envs => envs[env] || Envs.All;
+export const getCurrentEnv = (env = process.env.NODE_ENV): Envs => {
+  if (env) return envs[env] || Envs.Dev;
+  return Envs.Dev;
+};
