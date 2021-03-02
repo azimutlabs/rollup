29358742f5b9dba560fd8b2a3db55d145f70380e
olehan <alihan.arshabekov@gmail.com>
2021-03-02 14:29:50 +0600

    feat(rollup-config-essentials): add @rollup/plugin-alias to support absolute imports (#34)

--- packages/rollup-config-essentials/src/config.ts
+++ packages/rollup-config-essentials/src/config.ts
@@ -1,21 +1,25 @@
 import { RollupConfig } from '@azimutlabs/rollup-config';
 import { rollupPluginExternal } from '@azimutlabs/rollup-plugin-external';
+import alias from '@rollup/plugin-alias';
 import { DEFAULTS, nodeResolve } from '@rollup/plugin-node-resolve';
+import { lstatSync, readdirSync } from 'fs';
+import { resolve } from 'path';
 
 import type { RollupConfigEssentialsPlugins } from './types/RollupConfigEssentialsPlugins';
 
 export const rollupConfigEssentialsExtensions = DEFAULTS.extensions;
 
 export const rollupConfigEssentials = new RollupConfig<RollupConfigEssentialsPlugins>(
-  ({ rootDir }) => ({
-    external: [rollupPluginExternal, { packagePath: rootDir }],
-    nodeResolve: [
-      nodeResolve,
-      {
-        extensions: rollupConfigEssentialsExtensions,
-      },
-    ],
-  })
+  ({ rootDir, sourceDir }) => {
+    const entries = readdirSync(sourceDir)
+      .filter((dir) => lstatSync(resolve(sourceDir, dir)).isDirectory())
+      .map((find) => ({ find, replacement: resolve(sourceDir, find) }));
+    return {
+      alias: [alias, { entries }],
+      external: [rollupPluginExternal, { packagePath: rootDir }],
+      nodeResolve: [nodeResolve, { extensions: rollupConfigEssentialsExtensions }],
+    };
+  }
 );
 
 export const essentials = rollupConfigEssentials.finalize.bind(rollupConfigEssentials);
--- packages/rollup-config-essentials/src/types/RollupConfigEssentialsPlugins.ts
+++ packages/rollup-config-essentials/src/types/RollupConfigEssentialsPlugins.ts
@@ -1,7 +1,9 @@
 import type { RollupPluginExternalOptions } from '@azimutlabs/rollup-plugin-external';
+import type { RollupAliasOptions } from '@rollup/plugin-alias';
 import type { RollupNodeResolveOptions } from '@rollup/plugin-node-resolve';
 
 export type RollupConfigEssentialsPlugins = {
+  readonly alias: RollupAliasOptions;
   readonly external: RollupPluginExternalOptions;
   readonly nodeResolve: RollupNodeResolveOptions;
 };
