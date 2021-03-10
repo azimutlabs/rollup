b003ca426d8636f2d89d3be61dc7590203d14c62
olehan <alihan.arshabekov@gmail.com>
2021-03-10 14:44:12 +0600

    fix(rollup-config-babel): add root field as rootDir (#46)

--- packages/rollup-config-babel/src/config.ts
+++ packages/rollup-config-babel/src/config.ts
@@ -2,34 +2,36 @@ import {
   rollupConfigEssentials,
   rollupConfigEssentialsExtensions,
 } from '@azimutlabs/rollup-config-essentials';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
 import rollupPluginBabel from '@rollup/plugin-babel';
 
 import type { RollupConfigBabelPlugins } from './types/RollupConfigBabelPlugins';
 
 export const rollupConfigBabelExtensions = [
   ...rollupConfigEssentialsExtensions,
-  '.es6',
-  '.es',
-  '.jsx',
+  ...DEFAULT_EXTENSIONS,
   '.ts',
   '.tsx',
 ];
 
-export const rollupConfigBabel = rollupConfigEssentials.derive<RollupConfigBabelPlugins>(() => ({
-  external: {
-    checkForBabelMacro: true,
-  },
-  nodeResolve: {
-    extensions: rollupConfigBabelExtensions,
-  },
-  babel: [
-    rollupPluginBabel,
-    {
-      extensions: rollupConfigBabelExtensions,
-      skipPreflightCheck: true,
-      babelHelpers: 'runtime',
+export const rollupConfigBabel = rollupConfigEssentials.derive<RollupConfigBabelPlugins>(
+  ({ rootDir }) => ({
+    external: {
+      checkForBabelMacro: true,
     },
-  ],
-}));
+    nodeResolve: {
+      extensions: rollupConfigBabelExtensions,
+    },
+    babel: [
+      rollupPluginBabel,
+      {
+        babelHelpers: 'runtime',
+        skipPreflightCheck: true,
+        extensions: rollupConfigBabelExtensions,
+        root: rootDir,
+      },
+    ],
+  })
+);
 
 export const babel = rollupConfigBabel.finalize.bind(rollupConfigBabel);
