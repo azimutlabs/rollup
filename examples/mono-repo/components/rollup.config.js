5b7939f28d8a4cadb7aea80e628e74064fc3e933
olehan <alihan.arshabekov@gmail.com>
2021-04-09 00:39:33 +0600

    refactor: use esm configs for mono-repo example (#41)

--- examples/mono-repo/components/rollup.config.js
+++ examples/mono-repo/components/rollup.config.js
@@ -1,5 +1,5 @@
-const { combine } = require('@azimutlabs/rollup-config');
-const { babel } = require('@azimutlabs/rollup-config-babel');
-const { typescript } = require('@azimutlabs/rollup-config-typescript');
+import { combine } from '@azimutlabs/rollup-config';
+import { babel } from '@azimutlabs/rollup-config-babel';
+import { typescript } from '@azimutlabs/rollup-config-typescript';
 
-module.exports = combine(babel(), typescript())(__dirname);
+export default combine(babel(), typescript())(__dirname);
