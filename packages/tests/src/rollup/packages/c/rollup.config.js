f2489139c7210df216724f78a63f00747a303b3c
olehan <alihan.arshabekov@gmail.com>
2021-06-02 16:40:08 +0600

    refactor(tests): remove 'rollup' package tests

--- packages/tests/src/rollup/packages/c/rollup.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  input: 'c.js',
-  output: {
-    file: 'c.js',
-    format: 'cjs',
-  },
-};
-
-module.exports = [config, config];
--- packages/tests/src/rollup/packages/d/rollup.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  input: 'd.js',
-  output: {
-    file: 'd.js',
-    format: 'cjs',
-  },
-};
-
-module.exports = config;
